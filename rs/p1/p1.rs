19b93a8fe1dbbeec35408f46bfe130b139c2447e
i8abyte <imransalim08@gmail.com>
2024-02-09 06:40:31 +1300

    Refactor the solution written in Rust for problem 1 to use expressions instead of statements

--- rs/p1/p1.rs
+++ rs/p1/p1.rs
@@ -1,19 +1,26 @@
 fn main() {
-    let ceiling: i32 = 1000;
-    let sum: i32 = multiples_of_three_or_five(ceiling);
-    
-    println!("{}", sum);
+    println!("{}", multiples_of_three_or_five(3, 1000, 0))
 }
 
-fn multiples_of_three_or_five(ceiling: i32) -> i32 {
-    let mut y: i32 = 0;
-
-    for x in 3i32..ceiling {
-        if x % 3 == 0 || x % 5 == 0 {
-            y += x;
-        }
+fn multiples_of_three_or_five(start: i32, end: i32, mut sum: i32) -> i32 {
+    for x in start..end {
+        sum += three(x) | five(x)
     }
-    
-    return y;
+    sum
 }
 
+fn three(multiple: i32) -> i32 {
+    if multiple % 3 == 0 {
+        multiple
+    } else {
+        0
+    }
+}
+
+fn five(multiple: i32) -> i32 {
+    if multiple % 5 == 0 {
+        multiple
+    } else {
+        0
+    }
+}

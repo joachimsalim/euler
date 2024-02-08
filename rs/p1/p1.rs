0b25567c31fa7910ab88a694acda3522ac113631
i8abyte <imransalim08@gmail.com>
2024-02-09 07:44:03 +1300

    Refactor the functions of the solution written in Rust for problem 1

--- rs/p1/p1.rs
+++ rs/p1/p1.rs
@@ -4,23 +4,19 @@ fn main() {
 
 fn multiples_of_three_or_five(start: i32, end: i32, mut sum: i32) -> i32 {
     for x in start..end {
-        sum += three(x) | five(x)
+        sum += multiple_of_factor(x, 3) | multiple_of_factor(x, 5)
     }
     sum
 }
 
-fn three(multiple: i32) -> i32 {
-    if multiple % 3 == 0 {
+fn multiple_of_factor(multiple: i32, factor: i32) -> i32 {
+    if is_evenly_divisible(multiple, factor) {
         multiple
     } else {
         0
     }
 }
 
-fn five(multiple: i32) -> i32 {
-    if multiple % 5 == 0 {
-        multiple
-    } else {
-        0
-    }
+fn is_evenly_divisible(multiple: i32, factor: i32) -> bool {
+    multiple % factor == 0
 }

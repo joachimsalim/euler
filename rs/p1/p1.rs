629b7b5a5154fa9f402a644035f76680dd664af7
i8abyte <imransalim08@gmail.com>
2024-02-09 23:10:25 +1300

    Rename some components of the functions written in Rust for the solution of problem 1

--- rs/p1/p1.rs
+++ rs/p1/p1.rs
@@ -4,19 +4,20 @@ fn main() {
 
 fn multiples_of_three_or_five(start: i32, end: i32, mut sum: i32) -> i32 {
     for x in start..end {
-        sum += multiple_of_factor(x, 3) | multiple_of_factor(x, 5)
+        sum += product_of_factor(x, 3) | product_of_factor(x, 5)
     }
     sum
 }
 
-fn multiple_of_factor(multiple: i32, factor: i32) -> i32 {
-    if is_evenly_divisible(multiple, factor) {
-        multiple
+fn product_of_factor(multiplier: i32, factor: i32) -> i32 {
+    if is_evenly_divisible(multiplier, factor) {
+        multiplier
     } else {
         0
     }
 }
 
-fn is_evenly_divisible(multiple: i32, factor: i32) -> bool {
-    multiple % factor == 0
+fn is_evenly_divisible(dividend: i32, factor: i32) -> bool {
+    dividend % factor == 0
 }
+

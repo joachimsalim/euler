8c2e94acd06622d84caa30264073c714bbda5067
i8abyte <imransalim08@gmail.com>
2024-02-10 04:17:09 +1300

    Refactor the solution written in Rust for problem 1 to try and think in the functional programming paradigm

--- rs/p1/p1.rs
+++ rs/p1/p1.rs
@@ -4,20 +4,23 @@ fn main() {
 
 fn multiples_of_three_or_five(start: i32, end: i32, mut sum: i32) -> i32 {
     for x in start..end {
-        sum += product_of_factor(x, 3) | product_of_factor(x, 5)
+        sum += multiple(x, 3) | multiple(x, 5)
     }
     sum
 }
 
-fn product_of_factor(multiplier: i32, factor: i32) -> i32 {
-    if is_evenly_divisible(multiplier, factor) {
+fn multiple(multiplier: i32, multiplicand: i32) -> i32 {
+    if evenly_divisible(multiplier, multiplicand) {
         multiplier
     } else {
         0
     }
 }
 
-fn is_evenly_divisible(dividend: i32, divisor: i32) -> bool {
+fn evenly_divisible(dividend: i32, divisor: i32) -> bool {
     dividend % divisor == 0
 }
+
+
+// think about the order of arguments of a function in respect to its frame in the stack when called
 

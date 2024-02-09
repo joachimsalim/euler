3158b8ba11d609d0ace8ed32906bbd7778ca51bd
i8abyte <imransalim08@gmail.com>
2024-02-10 04:32:42 +1300

    Change the order of arguments of a function in the solution written in Rust for problem 1 to reason with infix notation

--- rs/p1/p1.rs
+++ rs/p1/p1.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("{}", multiples_of_three_or_five(3, 1000, 0))
 }
 
-fn multiples_of_three_or_five(start: i32, end: i32, mut sum: i32) -> i32 {
+fn multiples_of_three_or_five(mut sum: i32, start: i32, end: i32) -> i32 {
     for x in start..end {
         sum += multiple(x, 3) | multiple(x, 5)
     }
